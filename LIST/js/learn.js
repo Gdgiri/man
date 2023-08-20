3eca702bb64c6b12a4b213f3d8cc0f89b309b03e
Giridharan.S <96366499+Gdgiri@users.noreply.github.com>
2023-08-20 10:44:44 +0530

    Add files via upload
    save

---
+++ LIST/js/learn.js
@@ -0,0 +1,30 @@
+// function createAlertViaDOM(message) {
+//     const main = document.getElementById("main");
+  
+//     // Creating div element
+//     const div = document.createElement("div");
+//     const textNode = document.createTextNode(message);
+  
+//     // Assigning className
+//     div.className = "alert";
+  
+//     // Append
+//     div.append(textNode);
+//     main.prepend(div);
+//   }
+  
+//   createAlertViaDOM("Food is updated!");
+
+// const favouriteFoods=[
+//   "veg-briyaani",
+//   "kalan-briyaani",
+//   "cauliflower-briyaani"
+// ];
+
+// for(let i=0; i<favouriteFoods.length;i++){
+//   // console.log(`My favourite foods is: ${favouriteFoods[i]}`);
+//   document.write(favouriteFoods[i] );
+// }
+
+
+
